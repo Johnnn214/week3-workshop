216dcd78de6bcc7ae3bf1ca62af0aa5e8af9b24b
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-04 03:56:25 +1000

    updated server.js, added login and acount route,

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 
 var express = require('express'); //used for routing
+var formidable = require('formidable');
 var app = express();
 var http = require('http').Server(app); //used to provide http functionality
 
 app.use(express.static(__dirname + '/www'));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/www/index.html');
+    });
+
+app.get('/account', function (req, res) {
+    res.sendFile(__dirname + '/www/account.html');
+    });
 
 let server = http.listen(3000, function () {
     let host = server.address().address;
@@ -12,13 +22,11 @@ let server = http.listen(3000, function () {
     console.log("Server listening on: "+ host + " port: " + port);
     });
 
-app.get('/account', function (req, res) {
-    res.sendFile(__dirname + '/www/account.html');
-    });
+
     
-app.get('/test', function (req, res) {
-    res.sendFile(__dirname + '/www/test.html');
-    });
+// app.get('/test', function (req, res) {
+//     res.sendFile(__dirname + '/www/test.html');
+//     });
 
 
 
