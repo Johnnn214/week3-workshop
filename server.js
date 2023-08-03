d74788077905b58423d76a72db5a7bc588570fe2
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-04 07:26:05 +1000

    server.jsupdate

--- server.js
+++ server.js
@@ -2,26 +2,26 @@ var express = require('express'); //used for routing
 var app = express();
 var http = require('http').Server(app); //used to provide http functionality
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.urlencoded({ extended: true })); 
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/www/index.html');
     });
 
 app.post('/', function (req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    console.log('Received login request for email: ' + email + ', password: ' + password);
-    res.redirect('/returnlogin?email=${email}&password=${password}');
+    // const email = req.body.email;
+    // const password = req.body.password;
+    // res.redirect('/returnlogin?email={email}&password={password}');
   });
     
 
 
-// app.get('/returnlogin', function (req, res) {
-//     const email = req.query.email;
-//     const password = req.query.password;
-//     res.sendFile(__dirname + '/www/returnlogin.html');
-//     });
+app.get('/returnlogin', function (req, res) {
+    // const email = req.query.email;
+    // const password = req.query.password;
+    // res.sendFile(__dirname + '/www/returnlogin.html');
+
+    });
 
 app.get('/account', function (req, res) {
     res.sendFile(__dirname + '/www/account.html');
