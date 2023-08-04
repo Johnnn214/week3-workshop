b07df9bd07afa3d221612bd902c956aaf7ed9863
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-04 15:58:30 +1000

    added body parser, updated server.js, added script souce, added sprit.jd

--- server.js
+++ server.js
@@ -2,26 +2,41 @@ var express = require('express'); //used for routing
 var app = express();
 var http = require('http').Server(app); //used to provide http functionality
 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()) ;
+app.use(express.static(__dirname + '/www')); 
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/www/index.html');
     });
 
-app.post('/', function (req, res) {
-    // const email = req.body.email;
-    // const password = req.body.password;
-    // res.redirect('/returnlogin?email={email}&password={password}');
+app.post('/api/login', function (req, res) {
+    let users = [{'email':'john@com.au','password':'123'},{'email':'john@com.au','password':'123'},{'email':'john@com.au','password':'123'}]
+    if (!req.body){
+        return res.sendStatus(400)
+    }
+    console.log(req.body);
+    var user1 = {};
+    user1.email = req.body.email;
+    user1.password = req.body.password;
+    user1.valid = false;
+    for (let i=0; i<users.length; i++){
+        if (req.body.email == users[i].email && req.body.password == users[i].password){
+            user1.valid = true;
+        }
+    }
+    res.send(user1);
+
   });
     
 
 
-app.get('/returnlogin', function (req, res) {
-    // const email = req.query.email;
-    // const password = req.query.password;
-    // res.sendFile(__dirname + '/www/returnlogin.html');
+// app.get('/returnlogin', function (req, res) {
+//     const email = req.query.email;
+//     const password = req.query.password;
+//     res.sendFile(__dirname + '/www/returnlogin.html');
 
-    });
+//     });
 
 app.get('/account', function (req, res) {
     res.sendFile(__dirname + '/www/account.html');
---
+++ www/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    console.log("page ready");
+    $("#form").submit(function(event){
+        event.preventDefault();
+        ajaxPost();
+
+    });
+
+    function ajaxPost(){
+
+        var formData = {
+            email : $("#email").val(),
+            password: $("#password").val(),
+        }
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url : window.location +"api/login",
+            data: JSON.stringify(formData),
+            dataType : 'json',
+            success :function(user1){
+                console.log(user1);
+                $("#return").html("<p> " + "success <br>"+ "Email:"
+                + user1.email+ "<br>" +"valid:"+ user1.valid+ "</p>");
+
+            },
+            error : function(e){
+                alert("error")
+                console.log("ERROR: ", e);
+            }
+        });
+            resetData();
+    }
+        function resetData(){
+            $("#email").val("");
+            $("#password").val("");
+        }
+
+
+});
